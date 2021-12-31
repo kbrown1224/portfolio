4226c49c36dec799e1b7f5be9425b77c48364101
Kip Brown <krb24126@gmail.com>
2021-12-31 00:27:55 -0600

    Vercel deployment confs

--- quasar.conf.js
+++ quasar.conf.js
@@ -45,7 +45,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      distDir: ctx.mode.spa ? 'public' : null,
+      // distDir: ctx.mode.spa ? 'public' : null,
       // transpile: false,
       // publicPath: '/',
 
